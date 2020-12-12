00265ebd7e1bdda6bd843b013239e9c59b6071c9
Parodix007 <sebastian.siarczynski@gmail.com>
2020-12-12 01:02:46 +0100

    Implemetacja algorytmu pytajacego n razy baze danych i liczacego sredni wynik

--- db_conn/src/database.rs
+++ db_conn/src/database.rs
@@ -9,21 +9,39 @@ pub struct DataBase {
 impl DataBase {
 	pub fn new() -> Self {
 		Self {
-			conn_url: String::from("mysql://admin:1234qwer!@localhost:3306/employees"),
+			conn_url: String::from("mysql://root:haslo@localhost:3306/employees"),
 		}
 	}
-	pub fn get_actors(&self, query: &str){
-		#[derive(Debug)]
-		struct Actor {
-			first_name: String,
-			last_name: String,
-		}
+
+	pub fn make_request_and_get_time(&self, query: &str, n: i32) -> Vec<f32> {
 		let pool_conn = Pool::new(&self.conn_url).expect("Blad polaczenia");
 		let mut conn_to_db = pool_conn.get_conn().expect("Blad polaczenia");
-		let query = &conn_to_db.query_map(&query, |(first_name, last_name)| { 
-			Actor {first_name, last_name} 
-		}).expect("Blad kwerendy");
-		println!("{:#?}", query);
+		conn_to_db.query_drop("set profiling = 1").unwrap();
+		for _ in 0..n {	
+			conn_to_db.query_drop(&query).expect("Blad kwerendy");
+		}
+
+		let mut vec_of_time = vec![];
+
+		let _result:Vec<Row> = conn_to_db.query("show profiles").unwrap();
+		
+		for item in _result {
+			let value:String = item.get(1).unwrap();
+			let value:f32 = value.parse().unwrap();
+			vec_of_time.push(value);
+		}
+
+		vec_of_time
+	}
+
+	pub fn calc_avg(vec_with_result:Vec<f32>) -> f32{
+		let mut sum:f32 = 0.0;
+		for item in &vec_with_result{
+			sum += item;
+		}
+		let avg_time:f32 = sum / vec_with_result.len() as f32;
+		
+		avg_time
 	}
 }	
 
--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,6 +1,13 @@
 use db_conn::database::DataBase;
 
 fn main() {
-	let db = DataBase::new();
-    db.get_actors("SELECT * FROM employees")
+	let _db = DataBase::new();
+    let _result_of_query = _db.make_request_and_get_time("select salary, count(salary) as duplicates from salaries where salary = 66074", 2);
+    println!("{:?}", _result_of_query);
+    let _avg_time = DataBase::calc_avg(_result_of_query);
+    println!("{:?}", _avg_time);
+    let _result_of_query1 = _db.make_request_and_get_time("select salary, count(salary) as duplicates from salaries where salary = 71046", 2);
+    println!("{:?}", _result_of_query1);
+    let _avg_time1 = DataBase::calc_avg(_result_of_query1);
+    println!("{:?}", _avg_time1);
 }
